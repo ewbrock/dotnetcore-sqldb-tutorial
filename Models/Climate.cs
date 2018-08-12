c49a116627350984da7a311da7d852ccb6c146ed
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 11:49:28 -0400

    Update Climate.cs

--- Models/Climate.cs
+++ Models/Climate.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DotNetCoreSql.Models
+namespace DotNetCoreSqlDb.Models
 {
     public class Climate
     {
