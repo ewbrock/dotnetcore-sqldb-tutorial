39beec8f143fea9421272e0cfac90b34ce41b3dd
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 11:48:17 -0400

    Update Climate.cs

--- Models/Climate.cs
+++ Models/Climate.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WineTableMaint.Models
+namespace DotNetCoreSql.Models
 {
     public class Climate
     {
