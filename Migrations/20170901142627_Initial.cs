06da6b37f3868303b5f242b05750fc01d654e533
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 10:24:16 -0400

    Update 20170901142627_Initial.cs

--- Migrations/20170901142627_Initial.cs
+++ Migrations/20170901142627_Initial.cs
@@ -10,25 +10,34 @@ namespace DotNetCoreSqlDb.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Todo",
+                name: "Climate",
                 columns: table => new
                 {
-                    ID = table.Column<int>(nullable: false)
+                    ClimateID = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
                     Description = table.Column<string>(nullable: true)
+                    Rain_in_Winter = table.Column<string>(nullable: true)
+                    Rain_during_Growing_Season = table.Column<string>(nullable: true)
+                    Rain_during_Harvest = table.Column<string>(nullable: true)
+                    Temp_in_Winter = table.Column<string>(nullable: true)
+                    Temp_during_Growing_Season = table.Column<string>(nullable: true)
+                    Temp_Continentality = table.Column<string>(nullable: true)
+                    Temp_Diurnal = table.Column<string>(nullable: true)
+                    Vintage_Variation = table.Column<string>(nullable: true)
+                    Proximity_to_Large_Bodies_of_Water = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Todo", x => x.ID);
+                    table.PrimaryKey("PK_Climate", x => x.ClimateID);
                 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Todo");
+                name: "Climate");
         }
     }
 }
