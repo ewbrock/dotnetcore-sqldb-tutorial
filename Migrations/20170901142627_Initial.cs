e2f00b0b27e0e67cdedd522bfd7ee841d550f9b7
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 11:04:28 -0400

    Update 20170901142627_Initial.cs

--- Migrations/20170901142627_Initial.cs
+++ Migrations/20170901142627_Initial.cs
@@ -17,15 +17,15 @@ namespace DotNetCoreSqlDb.Migrations
                         .Annotation("Sqlite:Autoincrement", true)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
                     Name = table.Column<string>(nullable: false),
-                    Description = table.Column<string>(nullable: true)
-                    Rain_in_Winter = table.Column<string>(nullable: true)
-                    Rain_during_Growing_Season = table.Column<string>(nullable: true)
-                    Rain_during_Harvest = table.Column<string>(nullable: true)
-                    Temp_in_Winter = table.Column<string>(nullable: true)
-                    Temp_during_Growing_Season = table.Column<string>(nullable: true)
-                    Temp_Continentality = table.Column<string>(nullable: true)
-                    Temp_Diurnal = table.Column<string>(nullable: true)
-                    Vintage_Variation = table.Column<string>(nullable: true)
+                    Description = table.Column<string>(nullable: true),
+                    Rain_in_Winter = table.Column<string>(nullable: true),
+                    Rain_during_Growing_Season = table.Column<string>(nullable: true),
+                    Rain_during_Harvest = table.Column<string>(nullable: true),
+                    Temp_in_Winter = table.Column<string>(nullable: true),
+                    Temp_during_Growing_Season = table.Column<string>(nullable: true),
+                    Temp_Continentality = table.Column<string>(nullable: true),
+                    Temp_Diurnal = table.Column<string>(nullable: true),
+                    Vintage_Variation = table.Column<string>(nullable: true),
                     Proximity_to_Large_Bodies_of_Water = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
