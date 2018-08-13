e96346410061cea61fb0486531f3c9991c6720c8
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 23:55:16 -0400

    Update Index.cshtml.cs

--- Views/Climates/Index.cshtml.cs
+++ Views/Climates/Index.cshtml.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using WineTableMaint.Models;
+using DotNetCoreSqlDb.Models;
 
-namespace WineTableMaint.Pages.Climates
+namespace DotNetCoreSqlDb.Pages.Climates
 {
     public class IndexModel : PageModel
     {
-        private readonly WineTableMaint.Models.WineContext _context;
+        private readonly DotNetCoreSqlDb.Models.WineContext _context;
 
-        public IndexModel(WineTableMaint.Models.WineContext context)
+        public IndexModel(DotNetCoreSqlDb.Models.WineContext context)
         {
             _context = context;
         }
