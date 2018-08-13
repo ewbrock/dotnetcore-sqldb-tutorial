9baac9584500565e055b85abdff4f42f1fec6fdc
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 21:48:46 -0400

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WineTableMaint.Models
 
-namespace DotNetCoreSqlDb.Controllers
+namespace WineTableMaint.Controllers
 {
     public class HomeController : Controller
     {
