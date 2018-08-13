eb0786095bf5f11fa37faaac44b8abfd3001640c
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 22:09:19 -0400

    Update MyDatabaseContext.cs

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WineTableMaint.Models
+using WineTableMaint.Models;
 
 namespace WineTableMaint.Models
 {
