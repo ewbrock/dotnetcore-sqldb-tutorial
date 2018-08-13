13e16c8bd6361d36e221470aa574e9b6ae52c2a1
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 20:27:37 -0400

    Create Create.cshtml.cs

---
+++ Views/Climates/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WineTableMaint.Models;
+
+namespace WineTableMaint.Pages.Climates
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WineTableMaint.Models.WineTableMaintContext _context;
+
+        public CreateModel(WineTableMaint.Models.WineTableMaintContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Climate Climate { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Climate.Add(Climate);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
