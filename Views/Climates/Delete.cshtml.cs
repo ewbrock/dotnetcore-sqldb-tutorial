81eccce5ec561052b23e03cd8dbfe9c873660767
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 22:15:16 -0400

    Update Delete.cshtml.cs

--- Views/Climates/Delete.cshtml.cs
+++ Views/Climates/Delete.cshtml.cs
@@ -11,9 +11,9 @@ namespace WineTableMaint.Pages.Climates
 {
     public class DeleteModel : PageModel
     {
-        private readonly WineTableMaint.Models.WineContext _context;
+        private readonly WineTableMaint.Models.WineTableMaint _context;
 
-        public DeleteModel(WineTableMaint.Models.WineContext context)
+        public DeleteModel(WineTableMaint.Models.WineTableMaint context)
         {
             _context = context;
         }
