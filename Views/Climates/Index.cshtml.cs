fe413a9904ad7cb6f33bd3c69df850dec61fe7e0
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 20:32:26 -0400

    Create Index.cshtml.cs

---
+++ Views/Climates/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WineTableMaint.Models;
+
+namespace WineTableMaint.Pages.Climates
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WineTableMaint.Models.WineTableMaintContext _context;
+
+        public IndexModel(WineTableMaint.Models.WineTableMaintContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Climate> Climate { get;set; }
+
+        public async Task OnGetAsync(string searchString)
+        {
+            var climates = from m in _context.Climate
+                           select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                climates = climates.Where(s => s.Name.Contains(searchString));
+            }
+
+            Climate = await climates.ToListAsync();
+            
+        }
+    }
+}
