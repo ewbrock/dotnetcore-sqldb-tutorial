fe3b9e6a71b9c8484d472482b83da1898ebc90a6
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-13 00:11:08 -0400

    Update MyDatabaseContext.cs

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WineTableMaint.Models;
+using DotNetCoreSqlDb.Models;
 
-namespace WineTableMaint.Models
+namespace DotNetCoreSqlDb.Models
 {
-    public class MyDatabaseContext : DbContext
+    public class WineContext : DbContext
     {
         public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
             : base(options)
         {
         }
 
-        public DbSet<WineTableMaint.Models.Climate> Climate { get; set; }
+        public DbSet<DotNetCoreSqlDb.Models.Climate> Climate { get; set; }
     }
 }
