25feb1d11e17088e131cf48caad7ecf54c2cfc1d
ewbrock <42039326+ewbrock@users.noreply.github.com>
2018-08-12 23:03:10 -0400

    Update Details.cshtml.cs

--- Views/Climates/Details.cshtml.cs
+++ Views/Climates/Details.cshtml.cs
@@ -11,9 +11,9 @@ namespace WineTableMaint.Pages.Climates
 {
     public class DetailsModel : PageModel
     {
-        private readonly WineTableMaint.Models.WineTableMaint _context;
+        private readonly WineTableMaint.Models.WineContext _context;
 
-        public DetailsModel(WineTableMaint.Models.WineTableMaint context)
+        public DetailsModel(WineTableMaint.Models.WineContext context)
         {
             _context = context;
         }
